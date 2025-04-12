ca5c5eab6039d58d4d3b251976c4b8752d8c163f
Tplus <166980259+Tomais1@users.noreply.github.com>
2025-04-12 14:38:59 +0000

    se agregan los nodos

---
+++ Electo/src/Voto.java
@@ -0,0 +1,29 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class Voto {
+    private int id;
+    private int votanteId;
+    private int candidatoId;
+    private String timestamp;
+
+    public Voto(int id, int votanteId, int candidatoId) {
+        this.id = id;
+        this.votanteId = votanteId;
+        this.candidatoId = candidatoId;
+
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public int getVotanteId() {
+        return votanteId;
+    }
+
+    public int getCandidatoId() {
+        return candidatoId;
+    }
+}
+
