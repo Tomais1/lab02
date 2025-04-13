1827bdcbf25ce5bb2e1c36b224ddaf064573f3f5
Tplus <166980259+Tomais1@users.noreply.github.com>
2025-04-13 10:21:39 -0400

    Create Votante.java

---
+++ Electo/src/Votante.java
@@ -0,0 +1,37 @@
+public class Votante {
+    private int id;
+    private String nombre;
+    private Boolean yaVoto;
+
+    public Votante(int id,String nombre,Boolean yaVoto) {
+        this.id = id;
+        this.nombre = nombre;
+        this.yaVoto = yaVoto;
+        } 
+        //getters
+        public int getId() {
+            return id;
+        }
+        public String getNombre() {
+            return nombre;
+        }
+        public Boolean getyaVoto() {
+            return yaVoto;
+        }
+
+        // metodo para marcar los votos
+        public void marcarComovotado() {
+        this.yaVoto = true;
+        }
+
+        // setters
+        public void setId(int id) {
+            this.id = id;
+        }
+        public void setNombre(String nombre) {
+            this.nombre = nombre;
+        }
+        public void settaVoto(Boolean yaVoto) {
+            this.yaVoto = yaVoto;
+        }
+    }
